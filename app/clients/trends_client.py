cd24ff45ffd39fe68f9639bb3e80c93a625fde7d
SullivansAdvertisements <sullysmarketing7@gmail.com>
2025-12-21 01:07:49 -0500

    Update trends_client.py

--- app/clients/trends_client.py
+++ app/clients/trends_client.py
@@ -1,55 +1,21 @@
 from pytrends.request import TrendReq
-import pandas as pd
 
-def get_advanced_trends(
-    keywords,
-    geo="US",
-    timeframe="today 12-m",
-    gprop=""
-):
-    """
-    Returns:
-    - interest_over_time (DataFrame)
-    - interest_by_region (DataFrame)
-    - related_queries (dict)
-    """
-
-    if not keywords:
-        return {"error": "No keywords provided"}
-
+def google_youtube_trends(keywords, geo="US", timeframe="today 12-m"):
     pytrends = TrendReq(hl="en-US", tz=360)
-    pytrends.build_payload(
-        kw_list=keywords,
-        timeframe=timeframe,
-        geo=geo,
-        gprop=gprop,
-    )
+    pytrends.build_payload(keywords, geo=geo, timeframe=timeframe)
 
-    results = {}
+    interest = pytrends.interest_over_time()
+    regions = pytrends.interest_by_region(resolution="REGION", inc_low_vol=True)
 
-    # Interest over time
-    iot = pytrends.interest_over_time()
-    if not iot.empty and "isPartial" in iot.columns:
-        iot = iot.drop(columns=["isPartial"])
-    results["interest_over_time"] = iot
+    related = pytrends.related_queries()
+    rising = []
 
-    # Interest by region
-    region = pytrends.interest_by_region(
-        resolution="COUNTRY",
-        inc_low_vol=True,
-        inc_geo_code=True,
-    )
-    results["interest_by_region"] = region
+    for k, v in related.items():
+        if v.get("rising") is not None:
+            rising += v["rising"]["query"].tolist()
 
-    # Related queries
-    rq = pytrends.related_queries()
-    cleaned = {}
-
-    for kw, data in rq.items():
-        cleaned[kw] = {
-            "top": data["top"] if data["top"] is not None else pd.DataFrame(),
-            "rising": data["rising"] if data["rising"] is not None else pd.DataFrame(),
-        }
-
-    results["related_queries"] = cleaned
-    return results
+    return {
+        "interest_over_time": interest,
+        "regions": regions,
+        "rising_queries": list(set(rising))[:50],
+    }
